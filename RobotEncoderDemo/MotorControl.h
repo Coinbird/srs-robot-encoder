2b3dfb0f68db413741aea3766dc23e258b5ed33b
Will Smith <coinbird@gmail.com>
2020-02-14 00:20:53 -0800

    Add encoder interrupt support

--- RobotEncoderDemo/MotorControl.h
+++ RobotEncoderDemo/MotorControl.h
@@ -3,28 +3,79 @@
 #define LEFT_MOTOR_SERVO_PIN 13
 #define RIGHT_MOTOR_SERVO_PIN 12
 
+// Servo rates for stop (adjust via potentiometer on the servo if moving slowly)
 #define STOP_LEFT 1500
 #define STOP_RIGHT 1500
+
+// Encoders wired to these pins
+#define ENCODER_PIN_LEFT 3
+#define ENCODER_PIN_RIGHT 2
+
 Servo leftServo; 
 Servo rightServo; 
+
+// Encoder counters
+volatile int leftMotorDir = 1; // 1 = forward, -1 = reverse 
+volatile int leftEncoderCount = 0;
+volatile int rightMotorDir = 1;
+volatile int rightEncoderCount = 0;
+
+void leftCounter() {
+  // Called by interrrupt handler
+  leftEncoderCount += leftMotorDir;
+}
+
+void rightCounter() {
+  // Called by interrrupt handler
+    rightEncoderCount += rightMotorDir;
+}
+
+void resetEncoders() {
+  leftEncoderCount = 0;
+  rightEncoderCount = 0;  
+}
 
 void attachMotors() {
   rightServo.attach(RIGHT_MOTOR_SERVO_PIN);
   leftServo.attach(LEFT_MOTOR_SERVO_PIN);
+  pinMode(ENCODER_PIN_LEFT, INPUT);  
+  pinMode(ENCODER_PIN_RIGHT, INPUT);
+  attachInterrupt(digitalPinToInterrupt(ENCODER_PIN_LEFT), leftCounter, RISING);
+  attachInterrupt(digitalPinToInterrupt(ENCODER_PIN_RIGHT), rightCounter, RISING);  
+  resetEncoders();
 }
+
+void printEncoderCounts() {
+  Serial.print("Left counter ");
+  Serial.println(leftEncoderCount);
+  Serial.print("Right counter ");
+  Serial.println(rightEncoderCount);
+}
+
+
+// --- MOVEMENT COMMANDS ---
 
 void stopMotors() {
   leftServo.writeMicroseconds(STOP_LEFT);
   rightServo.writeMicroseconds(STOP_RIGHT);
 }
 
+void leftSpeed(int speed) {
+  leftMotorDir = speed > 0 ? 1 : -1; // set motor direction for interrupt encoder counter
+  leftServo.writeMicroseconds(STOP_LEFT + speed);  
+}
+
+void rightSpeed(int speed) {
+  rightMotorDir = speed > 0 ? 1 : -1;
+  rightServo.writeMicroseconds(STOP_RIGHT - speed);  // inverse of left
+}
+
 void testForward() {
-  leftServo.writeMicroseconds(STOP_LEFT + 100);
-  rightServo.writeMicroseconds(STOP_RIGHT - 100);  
+  leftSpeed(50);
+  rightSpeed(50);
 }
 
 void testBack() {
-  leftServo.writeMicroseconds(STOP_LEFT - 100);
-  rightServo.writeMicroseconds(STOP_RIGHT + 100);  
-  
+  leftSpeed(-50);
+  rightSpeed(-50);
 }
--- RobotEncoderDemo/RobotEncoderDemo.ino
+++ RobotEncoderDemo/RobotEncoderDemo.ino
@@ -3,24 +3,42 @@
  * SRS Robot Demo (Arduino Uno + ShieldBot, the older version of the kit)
  * Demo using Parallax Boe-Bot Digital Encoder Kit for PID motion control
  * Wired as shown here https://www.parallax.com/sites/default/files/downloads/28107-Boe-Bot-Digital-Encoder-Product-Guide-v2.0.pdf
+ * Details on interrupts here https://www.arduino.cc/reference/en/language/functions/external-interrupts/attachinterrupt/
  * Will Smith 2020
  */
 
 #include "MotorControl.h"
 
-int speed = 0;
+
+static int leftTarget = 100;
+static int rightTarget = 100;
 
 void setup() {
+  Serial.begin(115200);
+  Serial.print("Robot demo: Initializing...");
   attachMotors(); 
+  Serial.println("complete.");
 }
 
+
 void loop() {
+
+  // NOTE: If you are powering the robot over only USB, 
+  // the encoders may brown out and you'll get weird interrupt behavior.
+  // To test encoders, you may wish to power breadboard only (switch at 1) 
+  // and manually move the wheels.
+  
+  Serial.println("Moving Forward");
   testForward();
   delay(1000);
   stopMotors();
+  printEncoderCounts();
   delay(1000);
+
+  Serial.println("Moving Back");
   testBack();
   delay(1000);
   stopMotors();
+  printEncoderCounts();
   delay(1000);
 }
